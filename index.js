aa4812e4c1f808e057e2545d7a87606fe06e418b
eshanvitha <eshasai123@gmail.com>
2023-12-01 14:26:36 +0000

    why preview is not appearing

---
+++ index.js
@@ -0,0 +1,8 @@
+const element = (
+  <div className='bg-container'>
+    <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png' />
+    <img src='https://assets.ccbp.in/frontend/react-js/csk-img.png' />
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
